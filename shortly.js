b7148fd08ad78d18d6b23139826acfae801521e7
sofiamay <sofiamberlin@gmail.com>
2016-03-29 19:47:50 -0700

    Complete sprint

--- shortly.js
+++ shortly.js
@@ -4,6 +4,7 @@ var partials = require('express-partials');
 var bodyParser = require('body-parser');
 var bcrypt = require('bcrypt-nodejs');
 var session = require('express-session');
+var flash = require('connect-flash');
 
 
 var db = require('./app/config');
